e6afb45b26e2fe30ccd4cd7b0806b219c34c9c86
Henrique <henriquevaz03@gmail.com>
2025-04-24 13:29:16 -0300

    :construction: corrected files

--- scripts/BezierFitDemo.py
+++ scripts/BezierFitDemo.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import numpy as np
 from iguess0 import iguess0
 
--- scripts/iguess0.py
+++ scripts/iguess0.py
@@ -21,20 +21,15 @@ def iguess0(Q,k):
         k = defk(m, n)  # Calls for default knot position.
     dpkpc = k  # Position of knot points passed globally.
     P = knots(Q, k)  # call to compute the knotpoints.
-    print("Knots: ", P.T)
     dt = distEJL(P, n)  # Call to compute the distance between successive knot points.
     ang = tang(Q, k)  # Call to compute the angles for the unit tangent vectors.
     C = ctpts(P, ang, dt)  # Call to compute the control points for the curve.
     CT = C.T
-    print("Control: ", CT)
 
     unique_control = CT[~np.isin(CT, P).all(axis=1)]
-
-    # Keep the second and penultimate point
-    ctrl_pts = np.concatenate((unique_control[1:2], unique_control[-2:-1]))
+    
     # Removing commun points
     # Removing the first and last point
     ctrl_pts = np.array(unique_control)
-    print("ctrl_pts =", ctrl_pts)
     return ctrl_pts
     
